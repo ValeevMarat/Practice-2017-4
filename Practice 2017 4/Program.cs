ed0fb05e27b46a15ced0cf3b4bffbccf05c92ec9
user <user@LAPTOP-7JKVIGGR>
2017-07-06 17:57:58 +0500

    Добавьте файлы проекта.

---
+++ Practice 2017 4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// Задание №4 практики 2017г. 
+// Задание 119г: Вычислить бесконечную сумму с заданной точностью
+// Ссылка на "Задачи по программированию": http://aesa.kz:8081/computer-science/abramov.pdf
+
+namespace Practice_2017_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
